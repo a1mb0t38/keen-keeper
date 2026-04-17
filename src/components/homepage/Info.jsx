fbf1e166add915053feb83e9301d8e6a67a33de5
mursahaul islam noor <mursahaulnur17@gmail.com>
2026-04-17 11:04:12 +0600

    added dynamic info on info components

--- src/components/homepage/Info.jsx
+++ src/components/homepage/Info.jsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
+
+
+// const dataPromise = fetch('/friends.json').then(res=> res.json())
 
 const Info = () => {
+
+    // const freindsInfo = use(dataPromise);
+    // console.log(freindsInfo, "frends info");
+
+    const [friendsInfo, setFriendsInfo] = useState([]);
+
+    useEffect(()=>{
+        const fetchData = async () =>{
+            const res = await fetch('/friends.json');
+            const data = await res.json()
+            // console.log(data);
+            setFriendsInfo(data);
+        }
+        fetchData()
+    },[])
+
+    // console.log(friendsInfo);
+
+   
+
+    const onTrackCount = friendsInfo.filter(
+        friend => friend.status === "on-track"
+    ).length
+
+    const needAttention = friendsInfo.filter(
+        friend=> friend.status === "overdue" || friend.status === "almost due"
+    ).length
+
+
     return (
         <div>
             <div className=' max-w-[1110px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-10'>
                 <div className='bg-white shadow-sm p-8 text-center w-full'>
-                    <h1>0</h1>
+                    <h1>4</h1>
                     <p>Total Friends</p>
                 </div>
                 <div className='bg-white shadow-sm p-8 text-center w-full'>
-                    <h1>0</h1>
+                    <h1>{onTrackCount}</h1>
                     <p>On Track</p>
                 </div>
                 <div className='bg-white shadow-sm p-8 text-center w-full'>
-                    <h1>0</h1>
+                    <h1>{needAttention}</h1>
                     <p>Need Attention</p>
                 </div>
                 <div className='bg-white shadow-sm p-8 text-center w-full'>
-                    <h1>0</h1>
+                    <h1>{friendsInfo.length}</h1>
                     <p>Interactions This Month</p>
                 </div>
             </div>
